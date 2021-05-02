4a6069ea3a7d382b47bcd74952787b958803272c
Jack Wright <1998jwright@gmail.com>
2021-05-02 17:11:03 +0100

    improved queuelength command

--- src/commands/queuelength.js
+++ src/commands/queuelength.js
@@ -8,7 +8,8 @@ module.exports = {
 			const queue = getPlayer(msg.guild, bot);
 			const length = await queue.length();
 
-			if (length) msg.channel.send(result);
+			if (length) msg.channel.send(__.totalqueuelength(length));
+			else msg.channel.send(__.emptyqueue());
 		} catch (error) {
 			console.error(error);
 			msg.channel.send(__.commanderror());
