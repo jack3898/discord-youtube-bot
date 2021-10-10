ea8399c1fb0c189be20e1620675b7c0615726093
Carlos Eduardo <cadu97@gmail.com>
2021-10-10 18:41:23 -0300

    Implementing socket test

--- web/src/App.js
+++ web/src/App.js
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <div className="App">
-      <MenuNavbar />
+      <MenuNavbar socket={socket}/>
     </div>
   );
 }
--- web/src/MenuNavbar.js
+++ web/src/MenuNavbar.js
@@ -1,27 +1,36 @@
 import React from  'react';
 
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Container, Button } from 'react-bootstrap';
 
 class MenuNavbar extends React.Component {
+
+  constructor(props) {
+    super(props);
+    console.log(props);
+
+    this.state = {
+      socket: props.socket
+    };
+
+    this.playPause = this.playPause.bind(this)
+  }
+
+  playPause() {
+    this.state.socket.emit('teste', 'play');
+  }
+
   render() {
     return(
       <Navbar bg="light" expand="lg">
         <Container>
           <Navbar.Brand href="#home">RSP</Navbar.Brand>
+          <Navbar.Text>
+            Now playing: {this.props.socket !== null} Teste
+          </Navbar.Text>
+          <Navbar.Text>
+            <Button variant="primary" onClick={this.playPause}>>/||</Button>
+          </Navbar.Text>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
         </Container>
       </Navbar>
       )
